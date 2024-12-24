5eaf746f905cd2d6dad2c9a3a175ae2636407288
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-12-24 13:46:09 +0700

    add daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,28 @@ export default {
     "./resources/**/*.js",
     "./resources/**/*.vue",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: ["light", "dark", "cupcake", "synthwave"],
   },
-  plugins: [],
+  darkMode: ['selector'],
+  theme: {
+    extend: {
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
