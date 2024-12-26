2ac3bc56680825739b22242a534f5a919c53eb9e
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-12-27 00:14:56 +0700

    fix navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-base-300/70 fixed top-0 w-full z-50">
   <div class="navbar container mx-auto">
     <div class="flex-1">
-      <a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+      <a href="/" class="btn btn-ghost text-xl">Re:vielm</a>
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal px-1">
@@ -9,12 +9,15 @@
         <li><a href="/genres">Genres</a></li>
         <li>
           <details>
-            <summary>Account</summary>
+            <summary>
+              @auth
+              <p>Hello, <span class="text-accent">{{ Auth::user()->name }}</span></p>
+              @else
+              Account
+              @endauth
+            </summary>
             <ul class="bg-base-100 rounded-t-none p-2">
               @auth
-                <li>
-                  <p>Hello, <span class="text-accent">{{ Auth::user()->name }}</span></p>
-                </li>
                 <li class="p-0">
                   <form action="/logout" method="POST" class="p-0 w-full flex">
                     @csrf
