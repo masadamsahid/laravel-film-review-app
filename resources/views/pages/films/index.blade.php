5f53dda06c18399e99f30d8ffba101b25bc59d77
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-12-27 03:01:14 +0700

    fix film index

--- resources/views/pages/films/index.blade.php
+++ resources/views/pages/films/index.blade.php
@@ -8,9 +8,9 @@
   @endauth
   <main class="container grid grid-cols-5 p-4 mx-auto gap-3">
     @foreach ($films as $f)
-      <a href="/films/{{ $f->id }}" class="card bg-base-100 shadow-xl group overflow-hidden relative">
+      <a href="/films/{{ $f->id }}" class="card bg-base-100 shadow-xl group overflow-hidden relative h-96">
         <figure class="size-full">
-          <img src="{{ asset('uploads/' . $f->poster) }}" alt="{{ $f->title }}" class="size-full"/>
+          <img src="{{ asset('uploads/' . $f->poster) }}" alt="{{ $f->title }}" class="min-h-full min-w-full object-cover"/>
         </figure>
         <div class="card-body absolute -bottom-full group-hover:bottom-0 size-full z-10 bg-gradient-to-t from-black/80 flex flex-col justify-end transition-all duration-200">
           <h2 class="card-title">
