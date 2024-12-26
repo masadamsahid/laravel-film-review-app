846e368e5b8b2a37d8a84064d4ca5b47e9a2b876
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-12-27 02:08:57 +0700

    another fix for navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-base-300/70 fixed top-0 w-full z-50">
+<div class="bg-accent/95 text-base-100 fixed top-0 w-full z-50">
   <div class="navbar container mx-auto">
     <div class="flex-1">
       <a href="/" class="btn btn-ghost text-xl">Re:vielm</a>
@@ -11,17 +11,17 @@
           <details>
             <summary>
               @auth
-              <p>Hello, <span class="text-accent">{{ Auth::user()->name }}</span></p>
+              <p>Hello, <span class="underline font-semibold">{{ Auth::user()->name }}</span></p>
               @else
               Account
               @endauth
             </summary>
-            <ul class="bg-base-100 rounded-t-none p-2">
+            <ul class="bg-accent rounded-t-none p-2 w-full text-left">
               @auth
                 <li class="p-0">
                   <form action="/logout" method="POST" class="p-0 w-full flex">
                     @csrf
-                    <button type="submit" class="btn btn-error btn-sm w-full">Logout</button>
+                    <button type="submit" class="btn btn-error btn-sm w-full justify-start">Logout</button>
                   </form>
                 </li>
               @else
