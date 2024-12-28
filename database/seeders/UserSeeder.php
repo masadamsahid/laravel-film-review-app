35d8104e3237580532e8698ec79a44096277e67e
Maulana Adam Sahid <masadamsahid@gmail.com>
2024-12-28 16:53:00 +0700

    Change user seeder name 🤣

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,11 +16,11 @@ class UserSeeder extends Seeder
     {
         //
         DB::table("users")->upsert([
-            ["id" => 1, "name" => "user1", "email" => "user1@mail.com", "password" => Hash::make("password1"), "created_at" => now(), "updated_at" => now()],
-            ["id" => 2, "name" => "user2", "email" => "user2@mail.com", "password" => Hash::make("password2"), "created_at" => now(), "updated_at" => now()],
-            ["id" => 3, "name" => "user3", "email" => "user3@mail.com", "password" => Hash::make("password3"), "created_at" => now(), "updated_at" => now()],
-            ["id" => 4, "name" => "user4", "email" => "user4@mail.com", "password" => Hash::make("password4"), "created_at" => now(), "updated_at" => now()],
-            ["id" => 5, "name" => "user5", "email" => "user5@mail.com", "password" => Hash::make("password5"), "created_at" => now(), "updated_at" => now()],
+            ["id" => 1, "name" => "User Satu", "email" => "user1@mail.com", "password" => Hash::make("password1"), "created_at" => now(), "updated_at" => now()],
+            ["id" => 2, "name" => "User Dua", "email" => "user2@mail.com", "password" => Hash::make("password2"), "created_at" => now(), "updated_at" => now()],
+            ["id" => 3, "name" => "User Tiga", "email" => "user3@mail.com", "password" => Hash::make("password3"), "created_at" => now(), "updated_at" => now()],
+            ["id" => 4, "name" => "User Empat", "email" => "user4@mail.com", "password" => Hash::make("password4"), "created_at" => now(), "updated_at" => now()],
+            ["id" => 5, "name" => "User Lima", "email" => "user5@mail.com", "password" => Hash::make("password5"), "created_at" => now(), "updated_at" => now()],
         ], ["id"],);
     }
 }
